9544d999df9612f5ff762ebd2dfa6d9e425708e3
wch <1954860634@qq.com>
2016-11-16 09:56:31 +0800

    shushihua

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by moxuyou on 2016/11/16.
+//  Copyright © 2016年 moxuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by moxuyou on 2016/11/16.
+//  Copyright © 2016年 moxuyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
